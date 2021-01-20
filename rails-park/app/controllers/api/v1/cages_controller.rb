d2aa83c6987f7998badadccada58d6d1345504e3
stylo <pathanfaraaz@gmail.com>
2021-01-19 17:03:41 -0800

    skeleton functions set up in controllers

--- rails-park/app/controllers/api/v1/cages_controller.rb
+++ rails-park/app/controllers/api/v1/cages_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::CagesController < ApplicationController
+
+  # GET /cages
+  def index
+  end
+  # GET /cage/:id
+  def show
+  end
+  # POST /cages
+  def create
+  end
+  # PUT /cages/:id
+  def update
+  end
+  # DELETE /cages/:id
+  def destroy
+  end
+
+  private
+
+  def cage_params
+    params.require(:user).permit(:powerOn)
 end
--- rails-park/app/controllers/api/v1/dinos_controller.rb
+++ rails-park/app/controllers/api/v1/dinos_controller.rb
@@ -1,2 +1,27 @@
 class Api::V1::DinosController < ApplicationController
+  before_action :find_dino, only: [:show, :update, :desroy]
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def dino_params
+    params.require(:dino).permit(:name, :species, :isCarnivore, :cage_id)
+  end
+
+  def find_dino
+    @dino = Dino.find(params[:id])
 end
