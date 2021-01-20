13dfaca097dd6fc63490f91e49016147879a99fb
stylo <pathanfaraaz@gmail.com>
2021-01-19 17:17:05 -0800

    Fleshed out basic routes logic, need restrictions now.

--- rails-park/app/controllers/api/v1/cages_controller.rb
+++ rails-park/app/controllers/api/v1/cages_controller.rb
@@ -2,22 +2,46 @@ class Api::V1::CagesController < ApplicationController
 
   # GET /cages
   def index
+    @cages = Cage.all
+    render json: @cages
   end
   # GET /cage/:id
   def show
+    @cage = Cage.find(params[:id])
   end
   # POST /cages
   def create
+    @cage = Cage.new(cage_params)
+    if @cage.save
+      render json: @cage
+    else
+      render error: {error: 'Unable to create Cage.'}, status: 400
+    end
   end
   # PUT /cages/:id
   def update
+    @cage = Cage.find(params[:id])
+    if @cage
+      @cage.update(cage_params)
+      render json: {message: 'Cage successfully updated.'}, status: 200
+    else
+      render json: {error: 'Unable to update cage.'}, status:400
+    end
   end
   # DELETE /cages/:id
   def destroy
+    @cage = Cage.find(params[:id])
+    if @cage
+      @cage.destroy
+      render json: {message: 'cage successfully deleted.'}, status: 200
+    else
+      render json: {error: 'Unable to delete cage.'}, status: 400
+    end
   end
 
   private
 
   def cage_params
-    params.require(:user).permit(:powerOn)
+    params.require(:cage).permit(:maxCapacity, :currCapacity, :powerOn)
+  end
 end
--- rails-park/app/controllers/api/v1/dinos_controller.rb
+++ rails-park/app/controllers/api/v1/dinos_controller.rb
@@ -2,18 +2,39 @@ class Api::V1::DinosController < ApplicationController
   before_action :find_dino, only: [:show, :update, :desroy]
 
   def index
+    @dinos = Dino.all
+    render json: @dinos
   end
 
   def show
+    render json: @dino
   end
 
   def create
+    @dino = Dino.new(dino_params)
+    if @dino.save
+      render json: @dino
+    else
+      render error: {error: 'Unable to create dino.'}, status: 400
+    end
   end
 
   def update
+    if @dino
+      @dino.update(dino_params)
+      render json: {message: 'Dino successfully updated.'}, status: 200
+    else
+      render json: {error: 'Unable to update dino.'}, status:400
+    end
   end
 
   def destroy
+    if @dino
+      @dino.destroy
+      render json: {message: 'Dino successfully deleted.'}, status: 200
+    else
+      render json: {error: 'Unable to delete dino.'}, status: 400
+    end
   end
 
   private
@@ -24,4 +45,5 @@ class Api::V1::DinosController < ApplicationController
 
   def find_dino
     @dino = Dino.find(params[:id])
+  end
 end
